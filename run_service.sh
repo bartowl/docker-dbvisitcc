14b1900aafab4454c589a4f0d552143c08ef9d47
bartowl <github@bartowl.eu>
2022-07-25 15:22:38 +0200

    - supports pre-existing persisted volume

--- run_service.sh
+++ run_service.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-if ! [ -d /usr/dbvisit/persistent/db ]; then
-  echo "First-time execution, preparing persistent storage..."
-  mv /usr/dbvisit/standbymp/certificates /usr/dbvisit/persistent
-  mv /usr/dbvisit/standbymp/conf /usr/dbvisit/persistent
-  mv /usr/dbvisit/standbymp/db /usr/dbvisit/persistent
+if ! [ -l /usr/dbvisit/standbymp/db ]; then
+  if [ -d /usr/dbvisit/persistent/db ]; then
+    echo "Using persisted settings from from external volume..."
+    rm -rf /usr/dbvisit/standbymp/{certificates,conf,db}
+    AGENT_PASSWORD="persisted, do not re-generate"
+  else
+    echo "First-time execution, preparing persistent storage from template..."
+    mv /usr/dbvisit/standbymp/certificates /usr/dbvisit/persistent
+    mv /usr/dbvisit/standbymp/conf /usr/dbvisit/persistent
+    mv /usr/dbvisit/standbymp/db /usr/dbvisit/persistent
+  fi
   ln -s /usr/dbvisit/persistent/certificates /usr/dbvisit/standbymp/certificates
   ln -s /usr/dbvisit/persistent/conf /usr/dbvisit/standbymp/conf
   ln -s /usr/dbvisit/persistent/db /usr/dbvisit/standbymp/db
